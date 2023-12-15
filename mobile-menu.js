9ab89a0c837f95cdf302038fbde2d2727d28318e
AndreyMandybura <80692584+AndreyMandybura@users.noreply.github.com>
2023-12-15 14:12:46 +0200

    Меню закрывается при клике на кнопку

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,12 +1,17 @@
 "use strict";
 const menuBtnRef = document.querySelector("[data-menu-button]");
 const mobileMenuRef = document.querySelector("[data-menu]");
+const menuLinksRefs = document.querySelectorAll(".menu-list-link");
 
-menuBtnRef.addEventListener("click", () => {
-        const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+const toggleMenu = () => {
+  const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-        menuBtnRef.classList.toggle("is-open");
-        menuBtnRef.setAttribute("aria-expanded", !expanded);
+  menuBtnRef.classList.toggle("is-open");
+  menuBtnRef.setAttribute("aria-expanded", !expanded);
 
-        mobileMenuRef.classList.toggle("is-open");
-    });
+  mobileMenuRef.classList.toggle("is-open");
+};
+
+menuBtnRef.addEventListener("click", toggleMenu);
+
+menuLinksRefs.forEach((link) => link.addEventListener("click", toggleMenu));
